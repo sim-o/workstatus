e59bbbbd6720eb5487893e1c0fe569a47ed8a141
Simon Kerle <s.kerle@gmail.com>
2019-08-03 15:41:32 +1000

    init macos

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use std::sync::mpsc::channel;
+use crate::macos::OSXStatusBar;
 
 mod gitlab;
 mod macos;
@@ -7,5 +8,5 @@ fn main() {
     println!("merge requests: {:?}", gitlab::merge_request_count().unwrap());
 
     let (tx, rx) = channel::<String>();
-    let mut status = connectr::StatusBar::new(tx.clone());
+    let mut status = OSXStatusBar::new(tx.clone());
 }
