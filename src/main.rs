825d1fe9153c3bd5316c9c42405510f9a824f948
Simon Kerle <s.kerle@gmail.com>
2019-08-04 07:11:00 +1000

    sleep

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use std::sync::mpsc::channel;
 use crate::macos::{status};
+use std::{thread, time};
 
 mod gitlab;
 mod macos;
@@ -8,8 +9,15 @@ use std::sync::mpsc::Sender;
 pub type NSCallback = Box<dyn Fn(u64, &Sender<String>)>;
 
 fn main() {
-    println!("merge requests: {:?}", gitlab::merge_request_count().unwrap());
+//    println!("merge requests: {:?}", gitlab::merge_request_count().unwrap());
 
     let (tx, rx) = channel::<String>();
+
+    let child = thread::spawn(move || {
+        loop {
+            thread::sleep(time::Duration::from_millis(1000));
+        }
+    });
+
     status(tx);
 }
