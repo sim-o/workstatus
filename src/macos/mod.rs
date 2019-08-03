b3f3ec072149c338b037f832d02d3b56962981eb
Simon Kerle <s.kerle@gmail.com>
2019-08-03 17:50:49 +1000

    msg_Send

--- src/macos/mod.rs
+++ src/macos/mod.rs
@@ -8,7 +8,7 @@ extern crate fruitbasket;
 use self::fruitbasket::FruitApp;
 
 use objc::runtime::Class;
-use objc::{msg_send, sel};
+use objc::*;
 
 use self::cocoa::base::{nil, YES};
 use self::cocoa::appkit::NSStatusBar;
