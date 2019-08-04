f96e4435b79cfed530787641d361626c5ba4107f
Simon Kerle <s.kerle@gmail.com>
2019-08-04 13:42:07 +1000

    gitlab

--- src/main.rs
+++ src/main.rs
@@ -15,7 +15,21 @@ pub type NSCallback = Box<dyn Fn(u64, &Sender<String>)>;
 
 fn main() {
     let config = read_config().expect("error reading config.toml");
-    let worker_config = config.clone();
+    {
+        let worker_config = config.clone();
+        let (tx, rx) = channel::<String>();
+        let stopper = status_bar.stopper();
+        thread::spawn(move || {
+            loop {
+                if let Ok(result) = gl.merge_request_count(worker_config.ignore_users) {
+                    tx.send(format!("{:}: {:}", worker_config.project_name, result));
+                    stopper.stop();
+                }
+                thread::sleep(Duration::from_millis(60_000));
+            }
+        });
+    }
+
     let mut gl = Gitlab::new(
         config.gitlab_url.as_str(),
         config.token.as_str(),
@@ -38,17 +52,6 @@ fn main() {
         }
     });
 
-    let (tx, rx) = channel::<String>();
-    let stopper = status_bar.stopper();
-    thread::spawn(move || {
-        loop {
-            if let Ok(result) = gl.merge_request_count(worker_config.ignore_users) {
-                tx.send(format!("{:}: {:}", worker_config.project_name, result));
-                stopper.stop();
-            }
-            thread::sleep(Duration::from_millis(60_000));
-        }
-    });
 
     loop {
         status_bar.run(true);
